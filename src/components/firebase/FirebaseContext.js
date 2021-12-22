b74df2842138902581abc441480706abb661647c
dorian <dorianlegan@protonmail.com>
2021-12-22 00:00:42 +0000

    started refactoring image loading into hook

--- src/components/firebase/FirebaseContext.js
+++ src/components/firebase/FirebaseContext.js
@@ -1,24 +1,35 @@
 import { Firebase } from "./Firebase";
 import React, { useState, useEffect } from "react";
+import { useRef } from "react/cjs/react.development";
 const FirebaseContext = React.createContext();
 
 const FirebaseContextProvider = (props) => {
   const fb = Firebase();
   const { getAuth } = fb;
+  const [appUser, setAppUser] = useState(null);
   const [user, setUser] = useState(() => {
     const user = fb.getAuth().currentUser;
     return user;
   });
 
+  // remove all useage of user & replace with appuser
+  // change user to auth and ONLY use for auth
+
   const [loadingUser, setLoadingUser] = useState(true);
+  const [loadingUserProfile, setLoadingUserProfile] = useState(false);
   const [notificationsLoading, setNotificationsLoading] = useState(true);
   const [notificationData, setNotificationsData] = useState([]);
 
+  const isMounted = useRef(null);
+
   useEffect(() => {
+    isMounted.current = true;
     const loadNotifications = async (uid) => {
       const notificationData = await fb.getNotifications(uid);
-      setNotificationsData(notificationData);
-      setNotificationsLoading(false);
+      if (isMounted.current) {
+        setNotificationsData(notificationData);
+        setNotificationsLoading(false);
+      }
     };
 
     if (notificationsLoading && user != null) {
@@ -31,7 +42,28 @@ const FirebaseContextProvider = (props) => {
       setUser(firebaseUser);
       setLoadingUser(false);
     });
-  }, [getAuth]);
+  }, [appUser, fb, getAuth, user]);
+
+  useEffect(() => {
+    isMounted.current = true;
+    const getUserProfile = async () => {
+      setLoadingUserProfile(true);
+      const profile = await fb.getUserProfile(user.uid);
+
+      if (isMounted.current) {
+        setAppUser(() => {
+          return profile;
+        });
+        setLoadingUserProfile(false);
+      }
+    };
+    if (user && !loadingUserProfile && !appUser) {
+      getUserProfile();
+    }
+    if (!user) {
+      setAppUser(null);
+    }
+  }, [appUser, fb, loadingUserProfile, user]);
 
   return (
     <FirebaseContext.Provider
@@ -43,6 +75,7 @@ const FirebaseContextProvider = (props) => {
         submitComment: fb.addComment,
         notificationsLoading,
         notificationData,
+        appUser,
       }}
     >
       {props.children}
